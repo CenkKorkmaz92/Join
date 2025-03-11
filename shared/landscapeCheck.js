f9c2202d05f616bacd689c5207d1918821c66161
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-03-11 19:07:01 +0100

    fixed landscape warning

--- shared/landscapeCheck.js
+++ shared/landscapeCheck.js
@@ -16,7 +16,7 @@ function checkOrientation() {
     if (landscapeWarning) {
         // Show the warning only if it's a mobile device and in landscape mode
         if (isMobileDevice() && window.innerWidth > window.innerHeight) {
-            landscapeWarning.style.display = 'block';
+            landscapeWarning.style.display = 'flex';
         } else {
             landscapeWarning.style.display = 'none';
         }
