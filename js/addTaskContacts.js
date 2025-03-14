1a5c134037180035c64527a0f5eda5d1d834c324
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-03-14 14:59:05 +0100

    fixed assigned to checkbox fields, made it so you can click on the entire elemnt to add user

--- js/addTaskContacts.js
+++ js/addTaskContacts.js
@@ -41,8 +41,19 @@ function createContactListItem(contact) {
     item.appendChild(avatar);
     item.appendChild(label);
     item.appendChild(checkbox);
+
+    // Add click event listener to the whole item
+    item.addEventListener('click', (e) => {
+        // Prevent toggling twice if the click originates on the checkbox itself
+        if (e.target !== checkbox) {
+            checkbox.checked = !checkbox.checked;
+            toggleContact(contact, checkbox);
+        }
+    });
+
     return item;
 }
+
 
 function createAvatar(contact) {
     const avatar = document.createElement('div');
